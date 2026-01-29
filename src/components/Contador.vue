80be938e981b70e6b988876e34840b106ea17d5f
Dani-Obando29 <Obandodaniel29@outlook.com>
2026-01-29 16:36:48 -0600

    Inicio de componentes y reactivos

---
+++ src/components/Contador.vue
@@ -0,0 +1,29 @@
+<template>
+  <div style="display: flex">
+    <button @click="decremento">Decrementar</button>
+    <p>{{ contador }}</p>
+    <button @click="incremento">Incrementar</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const contador = ref(0);
+
+const incremento = () => {
+  //.value es la forma en la que cambiamos los valores de una variable reactiva u obtener su valor
+  contador.value++;
+};
+const decremento = () => {
+  //.value es la forma en la que cambiamos los valores de una variable reactiva u obtener su valor
+  contador.value--;
+};
+</script>
+
+<style scoped>
+p {
+  color: red;
+  padding: 20px;
+  font-size: 10px;
+}
+</style>
